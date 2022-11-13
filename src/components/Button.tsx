3af676bd14f48c5adabcb060d184efa1dbacf7a4
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2022-11-13 19:39:17 +0300

    create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import type { MouseEvent, PropsWithChildren } from "react";
+
+type ButtonProps = PropsWithChildren<{
+  onClick?: (e: MouseEvent) => void;
+}>;
+
+const StyledButton = styled.button`
+  background-color: green;
+`;
+
+export const Button = ({
+  children,
+  onClick = () => alert("Clicked"),
+}: ButtonProps) => {
+  return <StyledButton onClick={onClick}>{children}</StyledButton>;
+};
